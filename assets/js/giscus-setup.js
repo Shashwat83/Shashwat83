521723fb0a541bf2113daf0b5005f956ad7901aa
Shashwat83 <110742237+Shashwat83@users.noreply.github.com>
2025-11-10 13:20:34 +0000

    Deploying to gh-pages from @ Shashwat83/Shashwat83.github.io@0777704f2a421147523f3ac8b0615be3a8fca0f2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Shashwat83/Shashwat83",
+    "data-repo": "Shashwat83/Shashwat83.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
